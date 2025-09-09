1141818b6e95eea1a679026c3e737212e8735b56
Indah135 <indahamiati85@gmail.com>
2025-09-09 21:14:10 +0700

    update

--- js/script.js
+++
@@ -1,5 +0,0 @@
-// // Animasi saat submit
-// document.querySelector("form").addEventListener("submit", function(e) {
-//   e.preventDefault();
-//   alert("Terima kasih sudah mendaftar! 🎉");
-// });
